27fbc85b015eee6245c2c58fb6c7e1831c3955fa
Robin <105683782+rbncode@users.noreply.github.com>
2024-10-25 15:11:50 -0300

    styles: agregan estilos globales
    Se agregan estilos globales para la página de videojuego

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script>
+import Juegazo from '../components/Juegazo.vue';
+import Selector from '../components/Selector.vue'
+</script>
+
+<template>
+  <div class="about">
+    <Selector/>
+  </div>
+</template>
+
+<style>
+</style>
