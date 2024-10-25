475889346ed53a694b7942f5acc5d28163510446
Seba Yáñez <111529755+sebasufro@users.noreply.github.com>
2024-10-25 16:37:05 -0300

    actualiza estilos de landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,14 +3,40 @@
 </script>
 
 <template>
-    <nav>           
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" />
+    <nav class="navbar">
         <RouterLink to="repositorio">Repositorio</RouterLink>
         <RouterLink to="about">Sobre Juegazo</RouterLink>
         <RouterLink to="mechanics">Mecánicas</RouterLink>
+        <a class="login" href="#">Login</a>
     </nav>
 </template>
 
-<style>
+<style scoped>
+    .navbar {
+        padding: 5px;
+        width: 100% - 10px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        column-gap: 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-bottom: 20px;
+        background-color: var(--color-secondary);
+    }
 
+    a{
+        padding: 4px;
+        border-radius: 8px;
+        text-decoration: none;
+        color: var(--color-text);
+    }
+
+    a:hover{
+        color: var(--color-text-hover);
+    }
+
+    .login{
+
+    }
 </style>
--- src/views/Mecanicas.vue
+++ src/views/Mecanicas.vue
@@ -3,18 +3,40 @@
 </script>
 
 <template>
-      <div class="mechanics">
-    <h1>Personajes y Controles</h1>
-
-    <h1>
-        Controles
-    </h1>
-    <h2>Jugador 1</h2>
-    <p>WASD - movimiento</p>
-    <p>F - disparo</p>
-    <hr>
-    <h2>Jugador 2</h2>
-    <p>IJKL - movimiento</p>
-    <p>H - disparo</p>
+  <div class="mechanics">
+    <h1 class="titulo">Personajes y Controles</h1>
+    <h2 class="subtitulo">Controles</h2>
+    <div class="seccion">
+      <div class="jugador1">
+        <h3>Jugador 1</h3>
+        <p>WASD - movimiento</p>
+        <p>F - disparo</p>
+      </div>
+      <hr>
+      <div class="jugador2">
+        <h3>Jugador 2</h3>
+        <p>IJKL - movimiento</p>
+        <p>H - disparo</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .mechanics{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Arial, Helvetica, sans-serif;
+    row-gap: 10px;
+  }
+  .seccion{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    column-gap: 20px;
+  }
+  hr{
+    height: 60px;
+  }
+</style>
