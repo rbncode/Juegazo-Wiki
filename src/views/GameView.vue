d701f8f317c3f04a13ad0b2f1112945a7cfd1c4b
Robin <105683782+rbncode@users.noreply.github.com>
2024-11-03 14:45:37 -0300

    implementa componentes a vista

---
+++ src/views/GameView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import GameArea from "@/components/GameArea.vue";
+import HealthBar from "@/components/HealthBar.vue";
+</script>
+<template>
+  <HealthBar />
+  <GameArea />
+</template>
